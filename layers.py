fixed lstm backprop

layers.py
@@ -103,7 +103,7 @@ class LSTM_unit:
         self.dby = np.sum(dy, axis=0)
 
         dh += np.matmul(dy, self.Wy.T)
-        dc += dh * self.O
+        dc += dh * self.O * (1 - np.tanh(self.c) ** 2)
 
         dI = dc * self.G * self.I * (1 - self.I)
         dF = dc * self.c_prev * self.F * (1 - self.F)

models.py
@@ -122,7 +122,7 @@ class LSTM_manyToOne:
 
         if not self.stateful or self.final_h is None:
             self.final_h = np.zeros(shape=(N, self.lstm_Wh.shape[0])).astype('f')
-            self.final_c = np.zeros(shape=(N, self.lstm_Wh.shape[0]//4)).astype('f')
+            self.final_c = np.zeros(shape=(N, self.lstm_Wh.shape[0])).astype('f')
         
         for t in range(T):
             new_layer = layers.LSTM_unit(self.lstm_Wh, self.lstm_Wx, self.lstm_b, self.lstm_Wy, self.lstm_by)
@@ -138,7 +138,7 @@ class LSTM_manyToOne:
 
         if not self.stateful or self.final_h is None:
             self.final_h = np.zeros(shape=(N, self.lstm_Wh.shape[0])).astype('f')
-            self.final_c = np.zeros(shape=(N, self.lstm_Wh.shape[0]//4)).astype('f')
+            self.final_c = np.zeros(shape=(N, self.lstm_Wh.shape[0])).astype('f')
 
         for t in range(T):
             new_layer = layers.LSTM_unit(self.lstm_Wh, self.lstm_Wx, self.lstm_b, self.lstm_Wy, self.lstm_by)